cf26ec9cc4ce647191ee26057cef857d5e15dbe7
shanhuijie <shanhuijie@woncloud-sec.com>
2020-10-30 14:59:47 +0800

    test

--- main.go
+++ main.go
@@ -7,32 +7,55 @@ import(
 	"time"
 )
 func main(){
-	kill := core.InitList(50000)
-	var j,p int
+	kill := core.InitList(10000)
+	kill1 := core.InitList(10000)
+	kill2 := core.InitList(20000)
+	var j,p,p1,p2 int
 
 	go func(kill *core.Kill,j int,p int){
-		for j<2000000 {
+		for j<200000000 {
+			j++
+			kill.GetKillOption()
+		}
+	
+	}(kill1,j,p)
+	go func(kill *core.Kill,j int,p int){
+		for j<100000000 {
+			j++
+			kill.GetKillOption()
+		}
+	
+	}(kill2,j,p)
+	go func(kill *core.Kill,j int,p int){
+		for j<100000000 {
+			j++
+			kill.GetKillOption()
+		}
+	
+	}(kill2,j,p)
+	go func(kill *core.Kill,j int,p int){
+		for j<100000000 {
+			j++
+			kill.GetKillOption()
+		}
+	
+	}(kill2,j,p)
+	go func(kill *core.Kill,j int,p int){
+		for j<100000000 {
+			j++
+			kill.GetKillOption()
+		}
+	
+	}(kill2,j,p)
+	go func(kill *core.Kill,j int,p int){
+		for j<200000000 {
 			j++
 			kill.GetKillOption()
 		}
 	
 	}(kill,j,p)
 	go func(kill *core.Kill,j int,p int){
-		for j<1000000 {
-			j++
-			kill.GetKillOption()
-		}
-	
-	}(kill,j,p)
-	go func(kill *core.Kill,j int,p int){
-		for j<2000000 {
-			j++
-			kill.GetKillOption()
-		}
-	
-	}(kill,j,p)
-	go func(kill *core.Kill,j int,p int){
-		for j<2000000 {
+		for j<200000000 {
 			j++
 			kill.GetKillOption()
 		}
@@ -42,10 +65,14 @@ func main(){
 	for{
 		time.Sleep(3)
 		p = kill.List.Length()
-		if p == 0{
+		p1 = kill1.List.Length()
+		p2 = kill2.List.Length()
+		if p2 == 0{
 		 os.Exit(0)
 		}
-		fmt.Printf("i:%d \n",p)
+		fmt.Printf("i:%d ",p)
+		fmt.Printf("i1:%d ",p1)
+		fmt.Printf("i2:%d \n",p2)
 	}
 	
 }
